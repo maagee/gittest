830e6bca6a0ddef4f1d54f7ebf85f42c4104d6c8
zhangmintao <zhang.mintao@xbiao.com>
2024-02-23 15:55:23 +0800

    更新测试

--- test/src/views/AboutView.vue
+++ test/src/views/AboutView.vue
@@ -1,9 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 ref="title" v-if="show">{{str}}</h1>
+    <br>
+    <input type="text" v-model="list.data">
+    <button @click="modify">修改</button>
+    <button @click="del">卸载</button>
   </div>
 </template>
-
+<script setup>
+import { ref,reactive, onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted } from 'vue'
+const title = ref()
+const str = ref('This is an about page')
+const show = ref(true)
+let list = reactive({data:[1,2,3,4]})
+const modify = ()=>{
+  
+  setTimeout(()=>{
+    str.value = 'modify'
+    let arr = [9,8,7,6]
+    list.data.push(...arr)
+    console.log(list)
+  },1000)
+  
+}
+const del = ()=>{
+  show.value = false
+}
+onBeforeMount(()=>{
+  console.log('onBeforeMount:'+title.value)
+})
+onMounted(() => {
+  console.log('onMounted:'+title.value.innerText)
+})
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate:'+title.value.innerText)
+})
+onUpdated(()=>{
+  console.log('onUpdated:'+str.value)
+})
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount:'+str.value)
+})
+onUnmounted(() => {
+  console.log('onUnmounted:'+str.value)
+})
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
